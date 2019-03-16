9546e41bfcb13df097475e409b8b094408ad6c06
Flavio Bergamini <flaviohenrique@gec.inatel.br>
2019-03-16 13:40:28 -0300

    Criação da tela de agricultura, atualização da index.js e criação das telas de procedimento e ajuda

--- app1/src/Page3.js
+++ app1/src/Page3.js
@@ -1,12 +1,107 @@
-import React from 'react';
-import { View, Button, Text, StyleSheet,TouchableHighlight, ImageBackground, Platform } from 'react-native';
-import {Left,Right} from 'native-base';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, TouchableHighlight, ImageBackground} from 'react-native';
+import {Left, Right} from 'native-base';
+
+const Page3 = ({ navigation })=> (
+      <View style={{flex:16,backgroundColor:"#31bc84"}}>
+        
+        <View style={{flex:15 ,alignItems:'center',justifyContent:'center'}}>
+        
+        <TouchableHighlight style={styles.botao}onPress={()=>console.log('ok')}onPress={()=>navigation.navigate('Procedimentos')}>
+
+          <ImageBackground source={require('./Img/plantar.jpg')} style={{width: '100%', height: '100%',borderRadius:15}}>
+        
+            <Text style={{position:'relative',marginTop:100,marginLeft:100,color:'white',fontSize:20}}>COMO PLANTAR?</Text>
+          
+          </ImageBackground>
+
+        </TouchableHighlight>
+        
+        <TouchableHighlight style={styles.botao1}onPress={()=>console.log('ok')}onPress={()=>navigation.navigate('Ajuda')}>
+
+          <ImageBackground source={require('./Img/sei_plantar.jpg')} style={{width: '100%', height: '100%',borderRadius:15}}>
+
+            <Text style={{position:'relative',marginTop:100,marginLeft:27,color:'white',fontSize:20}}>SEI PLANTAR E QUERO AJUDAR!</Text>
+
+          </ImageBackground>
+
+        </TouchableHighlight>
 
 
-const Page3 = ({ navigation }) => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>About</Text>
-  </View>
+
+        </View>
+
+
+        <View style={{flex:1,flexDirection:'row'}}>
+
+    
+          <Left>
+            
+          <Text style={{marginLeft:60,fontSize:20, color:'white',alignItems:'flex-start',width:'50%',position:'relative'}}onPress={()=>this.sobre()}>Sobre</Text>
+
+          </Left>
+      
+          <Right>
+
+          <Text style={{fontSize:20,color:'white',alignItems:'flex-end',width:'50%',position:'relative'}}onPress={()=>this.saida()}>Sair</Text>
+
+          </Right>
+
+      
+        </View>
+      
+
+
+      </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  botao1:{
+    backgroundColor:'pink',
+    alignItems:'center',
+    justifyContent:'center',
+    width:390,
+    height:220,
+    position:'absolute',
+    top:260,
+    left:10,
+    borderRadius:15
+  },
+  botao:{
+    backgroundColor:'pink',
+    alignItems:'center',
+    justifyContent:'center',
+    width:390,
+    height:220,
+    position:'absolute',
+    top:20,
+    left:10,
+    borderRadius:15
+  }
+});
 
 export default Page3;
--- app1/src/index.js
+++ app1/src/index.js
@@ -4,6 +4,8 @@ import Page3 from './Page3';
 import Page4 from './Page4';
 import Page5 from './Page5';
 import Page6 from './Page6';
+import Page7 from './Page7';
+import Page8 from './Page8';
 
 import { createStackNavigator } from 'react-navigation';
 
@@ -14,6 +16,8 @@ const StackNavigator = createStackNavigator({
   Necessita: Page4,
   Doar_adotar: Page5,
   Local: Page6,
+  Procedimentos: Page7,
+  Ajuda: Page8,
 },{ headerMode: 'none'});
 
 export default { StackNavigator };
